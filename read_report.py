23108122a31417a99aa3e0f2f8d70a754596c032
David Chin <david.w.h.chin@gmail.com>
2022-04-08 17:03:17 -0400

    Deal with ranges of disk usage

--- read_report.py
+++ read_report.py
@@ -13,9 +13,10 @@ root = tree.getroot()
 print(root)
 print(f"root.attrib['time'] = {root.attrib['time']} = {delorean.epoch(int(root.attrib['time'])).shift('US/Eastern')}")
 
-MEGA = 1024 * 1024
-GIGA = MEGA * 1024
-TERA = GIGA * 1024
+KILO = 1024
+MEGA = KILO * KILO
+GIGA = MEGA * KILO
+TERA = GIGA * KILO
 
 
 for child in root:
@@ -27,7 +28,27 @@ for child in root:
                 print(f"group={gr_name} ({gid})")
                 for usage in domain.findall('usage'):
                     if usage.attrib['resource'] == 'physical':
-                        print(f"  physical usage = {int(usage.text)} Bytes = {float(usage.text)/TERA:.4e} TiB")
+                        du = int(usage.text)
+                        if du < KILO:
+                            print(f"  physical usage = {du} Bytes")
+                        elif du < MEGA:
+                            print(f"  physical usage = {du} Bytes = {float(du)/KILO:.4f} kiB")
+                        elif du < GIGA:
+                            print(f"  physical usage = {du} Bytes = {float(du)/MEGA:.4f} MiB")
+                        elif du < TERA:
+                            print(f"  physical usage = {du} Bytes = {float(du)/GIGA:.4f} GiB")
+                        else:
+                            print(f"  physical usage = {du} Bytes = {float(du)/TERA:.4f} TiB")
                     if usage.attrib['resource'] == 'logical':
-                        print(f"  logical usage = {int(usage.text)} Bytes = {float(usage.text)/TERA:.4e} TiB")
+                        du = int(usage.text)
+                        if du < KILO:
+                            print(f"  logical usage = {du} Bytes")
+                        elif du < MEGA:
+                            print(f"  logical usage = {du} Bytes = {float(du)/KILO:.4f} kiB")
+                        elif du < GIGA:
+                            print(f"  logical usage = {du} Bytes = {float(du)/MEGA:.4f} MiB")
+                        elif du < TERA:
+                            print(f"  logical usage = {du} Bytes = {float(du)/GIGA:.4f} GiB")
+                        else:
+                            print(f"  logical usage = {du} Bytes = {float(du)/TERA:.4f} TiB")
 
